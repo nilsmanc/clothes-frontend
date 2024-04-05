9c9d0a832a0d83c640e0bf5788d85e5298bf7942
nilsmanc <etereaiberia@gmail.com>
2024-04-05 09:15:13 +0300

    lang

---
+++ components/modules/Header/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container header_container">
+        <button className="btn-reset header_burger">
+          <span></span>
+          <span></span>
+        </button>
+      </div>
+    </header>
+  );
+};
