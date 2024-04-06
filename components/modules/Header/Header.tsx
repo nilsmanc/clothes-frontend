986bd060ef658d1211e330978bd30f83832d696b
nilsmanc <etereaiberia@gmail.com>
2024-04-06 08:18:29 +0300

    useLang

--- components/modules/Header/Header.tsx
+++ components/modules/Header/Header.tsx
@@ -1,12 +1,19 @@
+import { useLang } from "@/hooks/useLang";
+
 const Header = () => {
+  const { lang, translations } = useLang();
+
   return (
     <header className="header">
       <div className="container header_container">
         <button className="btn-reset header_burger">
-          <span></span>
-          <span></span>
+          <button className="btn-reset header_burger">
+            {translations[lang].header.menu_btn}
+          </button>
         </button>
       </div>
     </header>
   );
 };
+
+export default Header;
