de22e1f0bdfc8186bbe066e79dba92a14144c3d9
nilsmanc <etereaiberia@gmail.com>
2024-05-23 09:44:00 +0300

    user state

---
+++ context/user/index.ts
@@ -0,0 +1,26 @@
+"use client";
+import toast from "react-hot-toast";
+import { createDomain, createEffect } from "effector";
+import api from "@/api/apiInstance";
+import { setIsAuth } from "../auth";
+
+export const user = createDomain();
+
+export const loginCheck = user.createEvent<{ jwt: string }>();
+
+export const loginCheckFx = createEffect(async ({ jwt }: { jwt: string }) => {
+  try {
+    const { data } = await api.get("/api/users/login-check", {
+      headers: { Authorization: `Bearer ${jwt}` },
+    });
+
+    if (data?.error) {
+      return;
+    }
+
+    setIsAuth(true);
+    return data.user;
+  } catch (error) {
+    toast.error((error as Error).message);
+  }
+});
